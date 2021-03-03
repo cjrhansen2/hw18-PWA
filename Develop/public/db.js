f76b42569b3d8554fd8ea1c7bbf6015a5b93d22d
cjrhansen2 <cjrhansen2@gmail.com>
2021-03-02 18:00:49 -0800

    updated db .js

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -8,3 +8,26 @@ const indexedDB =
 
 let db;
 const request = indexedDB.open("budget", 1);
+
+//check database if successful
+request.onsuccess = ({ target }) => {
+    db = target.result;
+
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+}
+
+//upgrade if needed
+request.onupgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+//what to do if there's an error
+request.onerror = function(event) {
+    console.log("Woops! " + event.target.errorCode);
+};
+
+//event listener for the appp going online
+window.addEventListener("online", checkDatabase);
